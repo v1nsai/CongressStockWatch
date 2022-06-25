2b6a6bcdd89179eb763ce1db5da9f136203bd5f4
Tiffany Messer <tiffanymesser@tiffanys-air.mynetworksettings.com>
2022-06-25 14:01:56 -0400

    Initialized Springboot

---
+++ congressWatch/src/main/java/com/stockTracker/congressWatch/CongressWatchApplication.java
@@ -0,0 +1,13 @@
+package com.stockTracker.congressWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressWatchApplication.class, args);
+	}
+
+}
---
+++ congressWatch/src/test/java/com/stockTracker/congressWatch/CongressWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockTracker.congressWatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
