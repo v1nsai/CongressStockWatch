1122ca2a4e2c9d43f44d66dd9c40b6401dcf6466
Dr Ew <doctor_ew@protonmail.com>
2022-07-06 14:32:28 -0400

    WIP rebase scraper to selenium

--- elt/plugins/senate_scraper.py
+++ elt/plugins/senate_scraper.py
@@ -1,100 +1,32 @@
-from bs4 import BeautifulSoup
-import cloudscraper
-import requests
-import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from seleniumrequests import Chrome
+import time
 
-# def get_current_disclosures():
-# Get all new disclosures for today's date
+# init
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = Chrome(service=service) #webdriver.Chrome(service=service)
+url = "https://efdsearch.senate.gov/search/"
 
-# Retrieve the CSRF token and define constants
-session = requests.Session()
-url = 'https://efdsearch.senate.gov/search/'
-r = session.get(url)
-csrf_token = session.cookies.get('csrftoken')    
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Upgrade-Insecure-Requests": "1",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-User": "?1",
-    "Referer": f'{url}'
-}
-body = f"prohibition_agreement=1&first_name=&last_name=&filer_type=1&senator_state=&submitted_start_date=&submitted_end_date=&csrfmiddlewaretoken={csrf_token}"
+# Click the checkbox
+driver.get(url)
+# time.sleep(1)
+driver.find_element(By.ID, 'agree_statement').click()
 
-# Grab results
-response = session.post(url, body, headers)
-print()
+# Send the request
+driver.find_element(By.CSS_SELECTOR, '#filerTypes.form-check-input.senator_filer').click()
+driver.find_element(By.ID, 'fromDate').send_keys('01/01/2022')
+response = driver.find_element(By.CSS_SELECTOR, '.btn.btn-primary').click()
 
-
-# with open('test_scrape.json', 'w') as file:
-#     output_string = json.dumps(output).replace('},', '},\n')
-#     file.write(output_string)
+# Parse results
+table = driver.find_element(By.CSS_SELECTOR, '#filedReports_processing.dataTables_processing')
+headers = []
+theader = table.find_element(By.TAG_NAME, 'theader')
+tr = theader.find_elements(By.TAG_NAME, 'tr')
+for header in header_row:
+    headers.append(header.get_attribute('innerHTML'))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-def get_current_disclosures():
-    # Grab and parse main page
-    senate_stock_disclosures_url = 'https://sec.report/Senate-Stock-Disclosures/'
-
-    scraper = cloudscraper.create_scraper()
-    raw_text = scraper.get(senate_stock_disclosures_url).text
-    soup = BeautifulSoup(raw_text, features='lxml')
-
-    # Find the table and break it into rows
-    tbody = soup.select("#document_heading tbody")[0]
-
-    count = 1
-    output = []
-    for tr in tbody.find_all('tr'):
-        # Each row is actually 2 rows for some stupid reason, so grouping them with mod
-        if count % 2 == 1:
-            td = tr.find('td')
-            file_date = td.find('div', {"style": "float:left"}).text
-            transaction_date = td.find('div', {"style": "float:right"}).text
-            td = td.findNext('td')
-            issuer = td.a.text
-            td = td.findNext('td')
-            reporter = td.div.text.replace('\n', '')
-        if count % 2 == 0:
-            td = tr.find('td')
-            type = td.text.split(' ')
-            if len(type) == 2:
-                transaction_type = type[0]
-                transaction_hash = type[1]
-            if len(type) == 3:
-                transaction_type = f'{type[0]} {type[1]}'
-                transaction_hash = type[2]
-            td = td.findNext('td')
-            amount = td.text
-            td = td.findNext('td')
-            ownership = td.find('div', {"style": "float:right;"}).text
-            row = {
-                "file_date": file_date,
-                "transaction_date": transaction_date,
-                "issuer": issuer,
-                "reporter": reporter,
-                "transaction_type": transaction_type,
-                "transaction_hash": transaction_hash,
-                "amount": amount,
-                "ownership": ownership
-            }
-            output.append(row)
-        count = count + 1
-    
-    return output
+driver.quit()
